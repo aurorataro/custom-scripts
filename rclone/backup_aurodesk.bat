fd6b35dbbfe69ce27ef62be60f8155d19060f3b1
流云心 <aurorataro@foxmail.com>
2023-12-08 05:19:22 +0800

    Update backup_aurodesk.bat

--- rclone/backup_aurodesk.bat
+++ rclone/backup_aurodesk.bat
@@ -1,6 +1,7 @@
-if "%1"=="hide" goto CmdBegin
-start mshta vbscript:createobject("wscript.shell").run("""%~0"" hide",0)(window.close)&&exit
-:CmdBegin
-rclone sync "D:\Data\Aurorataro" onedrive:Public/Personal/Aurorataro --transfers 8
-rclone sync "D:\Data\FFXIV\FFXIV screenshot" onedrive:Public/Personal/Sync/FinalFantansy/screenshots --transfers 8
-rclone sync "E:\Game\FF14\   ’ª   XIV\game\My Games\FINAL FANTASY XIV - A Realm Reborn" onedrive:Public/Personal/Sync/FinalFantansy/config --filter-from "filter-list.txt" --transfers 8
+rclone sync "E:\Data\Aurorataro" onedrive:Public/Personal/Aurorataro --transfers 8 --progress
+rclone sync "E:\Data\FFXIV\FFXIV screenshot" onedrive:Public/Personal/Sync/FinalFantansy/screenshots --transfers 8 --progress
+rclone sync "D:\Games\FinalFantasy14\FF14\◊Ó÷’ª√œÎXIV\game\My Games\FINAL FANTASY XIV - A Realm Reborn" onedrive:Public/Personal/Sync/FinalFantansy/config --filter-from "filter-list.txt" --transfers 8 --progress
+rclone copy "E:\WindowsUserFile\Video" onedrive:Public/Windows/Video --transfers 8 --progress
+rclone sync "E:\WindowsUserFile\picture" onedrive:Public/Windows/Picture --transfers 8 --progress
+rclone sync "E:\WindowsUserFile\documents" onedrive:Public/Windows/Document --transfers 8 --progress
+rclone sync "E:\WindowsUserFile\Desktop" onedrive:Public/Windows/Desktop --transfers 8 --progress
